e4897e9fdb3346ade6860a14319f6033c5a0ac9f
irakli svanidze <irakli1393@gmail.com>
2017-02-06 15:34:39 +0400

    express sample

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'my-app',
 	template: `
-  
+  <h2>aaaaaaa</h2>
   <button type="button" class="btn btn-primary">Left</button>
   <button type="button" class="btn btn-default">Middle</button>
   <button type="button" class="btn btn-default">Right</button>
